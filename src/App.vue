5275bd853cce8fad9a6dc093fccf7a4b59d25397
André dos Santos <92130088+TW-Andre@users.noreply.github.com>
2025-02-10 10:08:45 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Toaster, toast } from 'vue-sonner'
+import index from './views/dashboard/index.vue'
+</script>
+
+<template>
+  <Toaster
+      richColors position="top-right"
+      :toastOptions="{
+            class: 'pe-2 ps-2 pt-5 pb-5',
+          }"
+  />
+  <index/>
+</template>
+
+<style scoped>
+
+</style>
